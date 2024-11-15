777b76ccd2015f51ce7bc3c90ed3d806f9543a07
Balmer Andres Arteaga Polo <balmerarteaga@gmail.com>
2024-11-15 16:34:36 -0500

    Saludo_Personal

---
+++ SaludoPersonal.java
@@ -0,0 +1,8 @@
+public class SaludoPersonal {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Balmer, bienvenido al curso de programacion en Java!!!");
+    }
+}
+
